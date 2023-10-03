212999399762d75cc31b20e331969d9a68e76716
deep237 <oppaagzt@gmail.com>
2023-10-03 14:55:09 +0200

    Update script.js

--- 2.fordulo/script.js
+++ 2.fordulo/script.js
@@ -1,9 +1,9 @@
 const position = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
 for (let x in position) {
-    $('td#2-' + position[x] + ' .space').prepend('<img id="dp' + x + '" data_color="white" data-piece="paraszt" data-x="2" data-y="' + x + '" src="chess_pieces/Chess_pdt45.svg" alt="d' + x + 'p">')
-    $('td#11-' + position[x] + ' .space').prepend('<img id="lp' + x + '" data_color="white" data-piece="paraszt" data-x="11" data-y="' + x + '" src="chess_pieces/Chess_plt45.svg" alt="d' + x + 'p">')
-
+    $('td#2-' + position[x] + ' .space').prepend('<img id="d' + x + 'p" data-color="black" data-piece="paraszt" data-x="' + x + '" data-y="1" src="chess_pieces/Chess_pdt45.svg" alt="d' + x + 'p">')
+    $('td#11-' + position[x] + ' .space').prepend('<img id="l' + x + 'p" data-color="white" data-piece="paraszt" data-x="' + x + '" data-y="10" src="chess_pieces/Chess_plt45.svg" alt="d' + x + 'p">')
+    
     $('td#2-' + position[x] + ' .space').addClass("piece");
     $('td#11-' + position[x] + ' .space').addClass("piece");
 }
@@ -26,10 +26,45 @@ $('td#12-' + position[5] + ' .space').prepend('<img id="dl5" data-color="white" 
 $('td#12-' + position[6] + ' .space').prepend('<img id="nl6" data-color="white" data-piece="huszar" data-x="6" data-y="11" src="chess_pieces/Chess_nlt45.svg" alt="nl6">');
 $('td#12-' + position[7] + ' .space').prepend('<img id="rl7" data-color="white" data-piece="bastya" data-x="7" data-y="11" src="chess_pieces/Chess_rlt45.svg" alt="rl7">');
 
+$('td#1-' + position[0] + ' .space').addClass("piece");
+$('td#1-' + position[1] + ' .space').addClass("piece");
+$('td#1-' + position[2] + ' .space').addClass("piece");
+$('td#1-' + position[3] + ' .space').addClass("piece");
+$('td#1-' + position[4] + ' .space').addClass("piece");
+$('td#1-' + position[5] + ' .space').addClass("piece");
+$('td#1-' + position[6] + ' .space').addClass("piece");
+
+
+$('td#12-' + position[0] + ' .space').addClass("piece");
+$('td#12-' + position[1] + ' .space').addClass("piece");
+$('td#12-' + position[2] + ' .space').addClass("piece");
+$('td#12-' + position[3] + ' .space').addClass("piece");
+$('td#12-' + position[4] + ' .space').addClass("piece");
+$('td#12-' + position[5] + ' .space').addClass("piece");
+$('td#12-' + position[6] + ' .space').addClass("piece");
+
 //todo: implement mozgas
 //kiraly: 2 minden iranyba
 //gyalog: elore 1,2,3  , utni atloban 1 lepesel
 
+function getPieceDatafromElement(e) {
+    return {
+        color : $(e).data("color"),
+        piece : $(e).data("piece"),
+        x : $(e).data("x"),
+        y : $(e).data("y")
+    }
+}
+
+function GetPieceByCoords(x, y) {
+    
+}
+
+$(".piece img").on( "click", function() {
+    let data = getPieceDatafromElement(this);
+    
+    console.log(data.y);
+});
 
 //fontos: azonositas kene a babuknak ;
 //hogy tudjuk oket hasznalni itt :)
