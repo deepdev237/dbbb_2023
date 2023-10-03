71c5c1cd90392f02cf3650a6823ae39c2d25aee5
alexluczi <luczialex@gmail.com>
2023-10-03 13:58:32 +0200

    Paraszt generator

--- 2.fordulo/script.js
+++ 2.fordulo/script.js
@@ -1,12 +1,15 @@
-// lerakja a gyalogokat bazdmeg
 const position = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
-for (let x in position) {
-    $('td#2-' + position[x] + ' .space').prepend('<img id="d' + x + '" src="chess_pieces/Chess_pdt45.svg" alt="">')
-}
+var pieces = []
+pieces.push({ id: "", categories: "", relase_date: "1996. május 22.", img: "chess_pieces/Chess_pdt45.svg", link: "https://www.imdb.com/title/tt0117060/" });
+
 
 for (let x in position) {
-    $('td#11-' + position[x] + ' .space').prepend('<img id="l' + x + '" src="chess_pieces/Chess_plt45.svg" alt="">')
+    $('td#2-' + position[x] + ' .space').prepend('<img id="d' + x + 'p" data_color="white" data-piece="paraszt" data-x="2" data-y="' + x + '" src="chess_pieces/Chess_pdt45.svg" alt="d' + x + 'p">')
+    $('td#11-' + position[x] + ' .space').prepend('<img id="l' + x + 'p" data_color="white" data-piece="paraszt" data-x="11" data-y="' + x + '" src="chess_pieces/Chess_plt45.svg" alt="d' + x + 'p">')
+
+    $('td#2-' + position[x] + ' .space').addClass("piece");
+    $('td#11-' + position[x] + ' .space').addClass("piece");
 }
 
 //todo: implement mozgas
