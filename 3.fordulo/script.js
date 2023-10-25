7e6d9c444ff4e50b4cb5592d81d5793821bba71c
deep237 <oppaagzt@gmail.com>
2023-10-25 22:04:53 +0200

    vege

--- 3.fordulo/script.js
+++ 3.fordulo/script.js
@@ -18,33 +18,55 @@ const decorationMargin = cakeSize * 0.02;
 const decorationX = cakeCenterX + cakeSize * 0.36 - decorationSize * 2 - decorationMargin * 2;
 const decorationY = cakeCenterY - cakeSize * 0.35 - decorationSize - decorationMargin;
 
-const numDecorations = 10;
+const submitButtonWidth = 150;
+const submitButtonHeight = 75;
+const submitButtonX = canvasWidth / 2 - submitButtonWidth / 2;
+const submitButtonY = canvasHeight - submitButtonHeight - 20;
+
+const generateButtonWidth = 150;
+const generateButtonHeight = 75;
+const generateButtonX = cakeCenterX - cakeSize * 0.36 - generateButtonWidth / 2;
+const generateButtonY = cakeCenterY + cakeSize * 0.35 + 20;
+
+const resetButtonWidth = 150;
+const resetButtonHeight = 75;
+const resetButtonX = cakeCenterX + cakeSize * 0.36 - resetButtonWidth / 2;
+const resetButtonY = cakeCenterY + cakeSize * 0.35 + 20;
+
+//const numDecorations = 10;
 const decorationRadius = cakeSize * 0.04;
 const circleRadius = cakeSize * 0.35;
 
-for (let i = 0; i < numDecorations; i++) {
-    let x, y, type;
-  
-    // Generate random coordinates and type until a non-overlapping decoration is found
-do {
-    let angle = Math.random() * 2 * Math.PI;
-    let radius = Math.random() * (circleRadius - decorationRadius * 2) + decorationRadius;
-    // Generate random x and y coordinates
-    x = cakeCenterX - cakeSize * 0.36 + radius * Math.cos(angle);
-    y = cakeCenterY + radius * Math.sin(angle);
-  
-    // Generate random decoration type
-    type = decorationTypes[Math.floor(Math.random() * decorationTypes.length)];
-  } while (decorations.some(decoration => Math.sqrt((decoration.x - x) ** 2 + (decoration.y - y) ** 2) < decorationRadius * 2) || Math.sqrt((x - cakeCenterX) ** 2 + (y - cakeCenterY) ** 2) < circleRadius + decorationRadius);
+function generateCake(amount) {
+    decorations = [];
+    usedDecorations = [];
+    matchingDecorations = [];
 
-    // Store decoration coordinates and type
-    decorations.push({ x, y, type });
+    for (let i = 0; i < amount; i++) {
+        let x, y, type;
+    
+        // Generate random coordinates and type until a non-overlapping decoration is found
+        do {
+            let angle = Math.random() * 2 * Math.PI;
+            let radius = Math.random() * (circleRadius - decorationRadius * 2) + decorationRadius;
+            // Generate random x and y coordinates
+            x = cakeCenterX - cakeSize * 0.36 + radius * Math.cos(angle);
+            y = cakeCenterY + radius * Math.sin(angle);
+        
+            // Generate random decoration type
+            type = decorationTypes[Math.floor(Math.random() * decorationTypes.length)];
+        } while (decorations.some(decoration => Math.sqrt((decoration.x - x) ** 2 + (decoration.y - y) ** 2) < decorationRadius * 2) || Math.sqrt((x - cakeCenterX) ** 2 + (y - cakeCenterY) ** 2) < circleRadius + decorationRadius);
+    
+        // Store decoration coordinates and type
+        decorations.push({ x, y, type });
+    }
 }
 
 //draw decorations at the top of the canvas where the player can choose from and drag and drop to the right circle to decorate it
 
 const decorationsToUse = [];
 let usedDecorations = [];
+let matchingDecorations = [];
 
 function main() {
     //Main loop
@@ -105,6 +127,61 @@ function main() {
         ctx.fill();
         ctx.closePath();
     }
+
+    //draw submit button on the bottom of the canvas
+
+    ctx.beginPath();
+    ctx.fillStyle = "green";
+    ctx.fillRect(submitButtonX, submitButtonY, submitButtonWidth, submitButtonHeight);
+    ctx.closePath();
+
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText("Kiértékelés", submitButtonX + submitButtonWidth / 2, submitButtonY + submitButtonHeight / 2);
+    ctx.closePath();
+
+    //draw generate button below the left circle on the canvas
+
+    ctx.beginPath();
+    ctx.fillStyle = "green";
+    ctx.fillRect(generateButtonX, generateButtonY, generateButtonWidth, generateButtonHeight);
+    ctx.closePath();
+
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText("Generálás", generateButtonX + generateButtonWidth / 2, generateButtonY + generateButtonHeight / 2);
+    ctx.closePath();
+
+    //draw reset button below the right circle on the canvas
+
+    ctx.beginPath();
+    ctx.fillStyle = "green";
+    ctx.fillRect(resetButtonX, resetButtonY, resetButtonWidth, resetButtonHeight);
+    ctx.closePath();
+
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText("Újra", resetButtonX + resetButtonWidth / 2, resetButtonY + resetButtonHeight / 2);
+    ctx.closePath();
+
+    //draw square on matchingDecorations 
+
+    for (const decoration of matchingDecorations) {
+        ctx.beginPath();
+        ctx.rect(decoration.x - decorationRadius, decoration.y - decorationRadius, decorationRadius * 2, decorationRadius * 2);
+        ctx.strokeStyle = "green";
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+
+
 }
 window.requestAnimationFrame(main); //Start the main loop
 
@@ -133,6 +210,9 @@ canvas.addEventListener("mousemove", (event) => {
 });
 
 canvas.addEventListener("mouseup", (event) => {
+    const x = event.offsetX;
+    const y = event.offsetY;
+
     if (selectedDecoration) {
         // Check if selected decoration is inside the right circle
         const distance = Math.sqrt((selectedDecoration.x - (cakeCenterX + cakeSize * 0.36)) ** 2 + (selectedDecoration.y - cakeCenterY) ** 2);
@@ -147,7 +227,46 @@ canvas.addEventListener("mouseup", (event) => {
     }
 
     selectedDecoration = null;
+
+    
+
+    if (x >= submitButtonX && x <= submitButtonX + submitButtonWidth && y >= submitButtonY && y <= submitButtonY + submitButtonHeight) {
+        // Submit button clicked
+        console.log("Submit button clicked!");
+
+        matchingDecorations = [];
+
+        for (let i = 0; i < usedDecorations.length; i++) {
+            const usedDecoration = usedDecorations[i];
+
+            for (let j = 0; j < decorations.length; j++) {
+            const decoration = decorations[j];
+
+            if (decoration.type === usedDecoration.type) {
+                const distance = Math.sqrt((decoration.x - usedDecoration.x) ** 2 + (decoration.y - usedDecoration.y) ** 2);
+
+                console.log(distance);
+                console.log(decorationRadius * 30);
+                if (distance < decorationRadius * 30) {
+                    matchingDecorations.push(decoration);
+                }
+            }
+            }
+        }
+
+        console.log(matchingDecorations);
+    }
+
+    if (x >= generateButtonX && x <= generateButtonX + generateButtonWidth && y >= generateButtonY && y <= generateButtonY + generateButtonHeight) {
+        // Generate button clicked
+        console.log("Generate button clicked!");
+        generateCake(2);
+    }
+
+    if (x >= resetButtonX && x <= resetButtonX + resetButtonWidth && y >= resetButtonY && y <= resetButtonY + resetButtonHeight) {
+        // Reset button clicked
+        console.log("Reset button clicked!");
+        usedDecorations = [];
+        matchingDecorations = [];
+    }
 });
-
-// Add event listeners to canvas for drag and drop
-
